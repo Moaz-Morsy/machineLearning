ecb7c01dfa0bbc0683a9ed74308ac5fd31bd504e
Raj Choudhary <raj.choudhary1603@gmail.com>
2018-06-08 21:50:06 +0530

    Random Forest Regression

--- Regression/RandomForestRegression/random_forest_regression.R
+++ Regression/RandomForestRegression/random_forest_regression.R
@@ -5,9 +5,25 @@ dataset = read.csv('Position_Salaries.csv')
 dataset = dataset[2:3]
 
 # Fitting the Random Forest Regression model to the dataset
-
+# install.packages('randomForest')
+library(randomForest)
+set.seed(1234)
+regressor = randomForest(x = dataset[1],
+                         y = dataset$Salary,
+                         ntree = 500)
 
 # Predicting results with the Random Forest Regression model
-
+y_pred = predict(regressor, newdata = data.frame(Level = 6.5))
 
 # Visualising the Random Forest Regression model with higher resolution
+# install.packages('ggplot2')
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+          colour = 'blue') +
+  ggtitle('Truth or Bluff (Random Forest Regression)') +
+  xlab('Position Level') +
+  ylab('Salary')
