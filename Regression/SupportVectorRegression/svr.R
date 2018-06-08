7e6617f8dd2b7ad73b65077a914993abb6f6d0e3
Raj Choudhary <raj.choudhary1603@gmail.com>
2018-06-08 08:54:57 +0530

    Support Vector Regression

--- Regression/SupportVectorRegression/svr.R
+++ Regression/SupportVectorRegression/svr.R
@@ -2,3 +2,39 @@
 
 # Importing the dataset
 dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Fitting SVR to the dataset
+# Here feature scaling is not required as e1071 has it in-built
+# install.packages('e1071')
+library(e1071)
+regressor = svm(formula = Salary ~ .,
+                data = dataset,
+                type = 'eps-regression')
+
+# Predicting a new result
+y_pred = predict(regressor, newdata = data.frame(Level = 6.5))
+
+# Visualising the SVR plot
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') + 
+  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (SVR)') +
+  xlab('Position Level') +
+  ylab('Salary')
+
+# Visualising the SVR model in higher resolution
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') + 
+  geom_line(aes(x = x_grid, y = predict(regressor,
+                                        newdata = data.frame(Level = x_grid))),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (SVR)') +
+  xlab('Position Level') +
+  ylab('Salary')
