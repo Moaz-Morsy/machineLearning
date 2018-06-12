79b54c613b19b22ed837fb22bbebcbba14e36f50
Raj Choudhary <raj.choudhary1603@gmail.com>
2018-06-12 13:17:50 +0530

    K-Means Clustering

---
+++ Clustering/KMeansClustering/k_means.R
@@ -0,0 +1,31 @@
+# K Means Clustering
+
+# Importing the dataset
+dataset = read.csv('Mall_Customers.csv')
+X = dataset[, 4:5]
+
+# Using Elbow Method to find the optimal number of clusters
+set.seed(6)
+wcss = vector()
+for (i in 1:10) wcss[i] = sum(kmeans(X, i)$withinss)
+plot(1:10, wcss, type = 'b', main = 'The Elbow Method', xlab = 'Number of Clusters', ylab = 'WCSS')
+
+# From the above plot, the optimum number of clusters is 5
+# Applying KMeans to the dataset
+set.seed(29)
+kmeans = kmeans(X, 5, iter.max = 300, nstart = 10)
+
+# Visualing the results of the Kmeans
+# install.packages('cluster')
+library(cluster)
+clusplot(X,
+         kmeans$cluster,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = 'KMeans Clustering',
+         xlab = 'Annual income',
+         ylab = 'Spending score')
